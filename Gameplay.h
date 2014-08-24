48ea657e2733696baab912019bc20b22a9ca703b
Van Doan <vdoan09@gmail.com>
2014-08-24 11:21:22 -0400

    added the function to allow the catapult to fire penguins

---
+++ Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  FlyingPens
+//
+//  Created by Van Doan  on 8/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
